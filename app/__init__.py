cc4963c516c5c81af3da2de3e4ce0d6c4cb49773
John Roller <jtroller33@gmail.com>
2023-02-06 21:18:51 -0600

    basic __init__ is done

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,31 @@
 # __init__.py does two things: contains Application Factory and tells python that the 'app/' folder should be treated as a package.
 
+import os
+
+from flask import Flask
+
+
+# create_app() is the application factory function
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'blog.sqlite'),    # path for DB
+    )
+    
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    @app.route('/hello')
+    def hello():
+        return 'sup'
+    
+    return app
