6ae0f89e89d4c8d3a857526f2984940393ff3eb4
John Roller <jtroller33@gmail.com>
2023-02-08 19:58:30 -0600

    added functions that run SQL commands

--- app/db.py
+++ app/db.py
@@ -14,8 +14,34 @@ def get_db():
         
     return g.db
 
+
 def close_db(e=None):
     db = g.pop('db', None)
     
     if db is not None:
         db.close()
+        
+        
+def init_db():
+    db = get_db()
+    
+    # .open_resource() opens a file relative to the `app` package (later when deployed may not know what the full path is)
+    # get_db returns a db connection
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+        
+        
+# this creates a CLI command 'init-db' that calls the init function and shows success message
+@click.command('init-db')
+def init_db_command():
+    """clears existing data and creates new tables"""
+    init_db()
+    click.echo('Initialized database')
+    
+    
+def init_app(app):
+    # tells Flask to call `close_db()` after response when cleaning up
+    app.teardown_appcontext(close_db)
+    
+    # adds a new command that can be called with the `flask` command
+    app.cli.add_command(init_db_command)
