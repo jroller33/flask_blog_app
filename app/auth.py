d8a044ff746dcc003a3e77fb57146f44d194996d
John Roller <jtroller33@gmail.com>
2023-02-08 20:14:23 -0600

    auth blueprint is registered with factory function

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,7 @@
+# blueprint is a way to organize a group of related views. blueprint is available in factory function
+# this app will have 2 blueprints: one for auth functions, one for blog post functions. code for each bp goes in separate modules.
+
+
 import functools
 
 from flask import (
@@ -7,7 +11,5 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.db import get_db
 
+# creates a blueprint named 'auth'. needs to be imported and registered with the factory fn using app.register_blueprint()
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-# blueprint is a way to organize a group of related views. blueprint is available in factory function
-# this app will have 2 blueprints: one for auth functions, one for blog post functions. code for each blueprint goes in separate modules.
