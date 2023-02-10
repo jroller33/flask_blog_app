2b2afe6465fb6efcfbace4c043065592ced5b54a
John Roller <jtroller33@gmail.com>
2023-02-10 12:01:03 -0600

    AuthActions class

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,24 @@ from app.db import get_db, init_db
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
 
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
+
 
 @pytest.fixture
 def app():
