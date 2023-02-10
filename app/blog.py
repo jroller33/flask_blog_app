8eff3f4844b5675891ddd9ef12a779639ce67f67
John Roller <jtroller33@gmail.com>
2023-02-10 11:32:01 -0600

    get_post() is for UPDATE and DELETE views

--- app/blog.py
+++ app/blog.py
@@ -8,6 +8,23 @@ from app.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
 @bp.route('/')
 def index():
     db = get_db()
