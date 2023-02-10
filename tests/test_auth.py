55e2633ab90ad9993494b11d96ec1a83f7bc7bf9
John Roller <jtroller33@gmail.com>
2023-02-10 12:04:16 -0600

    logout tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -14,7 +14,23 @@ def test_register(client, app):
         assert get_db().execute(
             "SELECT * FROM user WHERE username = 'a'",
         ).fetchone() is not None
-
+        
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
+    
+    
+    
+    
+    
 def test_login(client, auth):
     assert client.get('/auth/login').status_code == 200
     response = auth.login()
@@ -35,14 +51,9 @@ def test_login_validate_input(auth, username, password, message):
     assert message in response.data
     
     
-@pytest.mark.parametrize(('username', 'password', 'message'), (
-    ('', '', b'Username is required.'),
-    ('a', '', b'Password is required.'),
-    ('test', 'test', b'already registered'),
-))
-def test_register_validate_input(client, username, password, message):
-    response = client.post(
-        '/auth/register',
-        data={'username': username, 'password': password}
-    )
-    assert message in response.data
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert 'user_id' not in session
