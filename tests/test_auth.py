721fb3135f63f3af049c5858468cc2b5ae1c5294
John Roller <jtroller33@gmail.com>
2023-02-10 12:02:46 -0600

    login tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -15,7 +15,26 @@ def test_register(client, app):
             "SELECT * FROM user WHERE username = 'a'",
         ).fetchone() is not None
 
+def test_login(client, auth):
+    assert client.get('/auth/login').status_code == 200
+    response = auth.login()
+    assert response.headers["Location"] == "/"
 
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username.'),
+    ('test', 'a', b'Incorrect password.'),
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
+    
+    
 @pytest.mark.parametrize(('username', 'password', 'message'), (
     ('', '', b'Username is required.'),
     ('a', '', b'Password is required.'),
