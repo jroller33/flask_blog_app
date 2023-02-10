796efb8b589536fe4ad80b62e283b05caf85e072
John Roller <jtroller33@gmail.com>
2023-02-10 11:59:50 -0600

    db tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -13,3 +13,16 @@ def test_get_close_db(app):
         db.execute('SELECT 1')
 
     assert 'closed' in str(e.value)
+    
+    
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
